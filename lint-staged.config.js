88953946649a04526f29209946f52c3d3843c41e
Arthur Ming <guobin.ming@pwc.com>
2022-11-18 16:20:12 +0800

    feat: bump angular v15

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,13 +4,9 @@ module.exports = {
   "projects/ngx-is-standalone/src/**/*.ts?(x)": (filenames) =>
     filenames.length > 0
       ? "ng lint  ngx-is-standalone"
-      : `tslint --format verbose --project ./projects/ngx-is-standalone/tsconfig.spec.json --config ./projects/ngx-is-standalone/tslint.json ${filenames.join(
-          " "
-        )}`,
+      : `eslint --format stylish ${filenames.join(" ")}`,
   "src/**/*.ts?(x)": (filenames) =>
     filenames.length > 10
       ? "ng lint ngx-is-standalone-demo"
-      : `tslint --format verbose --project ./tsconfig.json --config ./tslint.json ${filenames.join(
-          " "
-        )}`,
+      : `eslint --format stylish ${filenames.join(" ")}`,
 };
